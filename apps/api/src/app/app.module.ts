347e187c464ff0c42b17dea2ff0ef824e4f2799f
Michał Dyrcz <mdyrcz5@gmail.com>
2021-11-08 22:54:09 +0100

    feature create data module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DataModule } from '@playground-ng-nest/data';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,7 +6,7 @@ import { CoreModule } from '@playground-ng-nest/core';
 import { RestaurantModule } from '@playground-ng-nest/restaurant';
 
 @Module({
-  imports: [CoreModule, RestaurantModule],
+  imports: [CoreModule, RestaurantModule, DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ libs/data/src/lib/data.service.ts
@@ -0,0 +1,44 @@
+import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleDestroy, OnModuleInit, Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class DataService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly admin_user: { email: string; password: string } = {
+    email: this.config.get('admin').email,
+    password: this.config.get('admin').password,
+  };
+
+  constructor(private readonly config: ConfigService) {
+    super();
+  }
+
+  public async onModuleInit() {
+    await this.$connect();
+    await this.ensureAdminUserExists();
+  }
+
+  public async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  public async createUser(email: string, password: string) {
+    const user = await this.user.create({ data: { email, password } });
+  }
+
+  private async findUserByEmail(email: string) {
+    return this.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  private async ensureAdminUserExists(): Promise<void> {
+    const foundedUser = this.findUserByEmail(this.admin_user.email);
+    if (!foundedUser) {
+      await this.createUser(this.admin_user.email, this.admin_user.password);
+    }
+    Logger.log('User already exists in DB');
+  }
+}
