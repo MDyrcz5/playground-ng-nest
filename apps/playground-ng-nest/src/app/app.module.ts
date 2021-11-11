ee5ba12178db263ffa0da32856f80a31dd5c26d0
Michał Dyrcz <mdyrcz5@gmail.com>
2021-11-11 20:04:56 +0100

    Add angular material

--- apps/playground-ng-nest/src/app/app.module.ts
+++ apps/playground-ng-nest/src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component'
-import { RouterModule } from '@angular/router'
-
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    DragDropModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
